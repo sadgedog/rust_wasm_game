29404a34c8302874b8e3d131d89009a0e8c46c2a
sadgedog <sorachi1231.214@gmail.com>
2023-12-30 13:02:08 +0900

    sierpinski

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,34 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]) {
+    let [top, left, right] = points;
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
+}
+
+fn mid_point(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
+    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
+
+fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], depth: u8) {
+    let [top, left, right] = points;
+    draw_triangle(context, points);
+    let depth = depth - 1;
+    if depth > 0 {
+        let left_middle = mid_point(top, left);
+        let right_middle = mid_point(top, right);
+        let bottom_middle = mid_point(left, right);
+        sierpinski(context, [top, left_middle, right_middle], depth);
+        sierpinski(context, [left_middle, left, bottom_middle], depth);
+        sierpinski(context, [right_middle, bottom_middle, right], depth);
+    }
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -28,9 +56,9 @@ pub fn main_js() -> Result<(), JsValue> {
     let document = window.document().unwrap();
     let canvas = document
         .get_element_by_id("canvas")
-        .unwrap()
+	    .unwrap()
         .dyn_into::<web_sys::HtmlCanvasElement>()
-        .unwrap();
+    	.unwrap();
 
     let context = canvas
         .get_context("2d")
@@ -39,14 +67,8 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
     
-    context.move_to(300.0, 0.0);
-    context.begin_path();
-    context.line_to(0.0, 600.0);
-    context.line_to(600.0, 600.0);
-    context.line_to(300.0, 0.0);
-    context.close_path();
-    context.stroke();
-    context.fill();
+    // draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
+    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 10);
     
     Ok(())
 }
