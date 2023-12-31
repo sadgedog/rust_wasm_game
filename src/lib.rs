dc8478af082789f85db4999d978b13e31eb4214d
sadgedog <sorachi1231.214@gmail.com>
2023-12-31 18:04:23 +0900

    sprite_2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
-use std::{collections::HashMap, rc::Rc, sync::Mutex};
 use rand::prelude::*;
+use serde::Deserialize;
+use std::{collections::HashMap, rc::Rc, sync::Mutex};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
+
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
+
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+}
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -12,74 +31,21 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    // await? : Result<JsValue, JsValue> -> JsValue
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
 
-fn mid_point(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    let [top, left, right] = points;
-    draw_triangle(context, points, color);
-    let depth = depth - 1;
-    if depth > 0 {
-        let mut rng = thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-
-        let left_middle = mid_point(top, left);
-        let right_middle = mid_point(top, right);
-        let bottom_middle = mid_point(left, right);
-        sierpinski(context, [top, left_middle, right_middle], next_color, depth);
-        sierpinski(
-            context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth,
-        );
-    }
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    // #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Web Assembly!!"));
+    console::log_1(&JsValue::from_str("Hello Web Assembly!"));
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
@@ -96,8 +62,17 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-
     wasm_bindgen_futures::spawn_local(async move {
+        let json = fetch_json("rhb.json")
+            .await
+            .expect("Could not fetch rhb.json");
+
+        let sheet: Sheet = json
+            .into_serde()
+            .expect("Could not convert rhb.json into a Sheet structure");
+
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
         let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
         let success_tx = Rc::new(Mutex::new(Some(success_tx)));
         let error_tx = Rc::clone(&success_tx);
@@ -117,20 +92,37 @@ pub fn main_js() -> Result<(), JsValue> {
 
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
         image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
-        
+        image.set_src("rhb.png");
         success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-    
-        // draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
-        sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );    
-    });
 
+        let mut frame = -1;
+        let interval_callback = Closure::wrap(Box::new(move || {
+            frame = (frame + 1) % 8;
+            let frame_name = format!("Run ({}).png", frame + 1);
+            context.clear_rect(0.0, 0.0, 600.0, 600.0);
+            let sprite = sheet.frames.get(&frame_name).expect("Cekk not found");
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                sprite.frame.x.into(),
+                sprite.frame.y.into(),
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+                300.0,
+                300.0,
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+            );
+    
+        }) as Box<dyn FnMut()>);
+
+        window.set_interval_with_callback_and_timeout_and_arguments_0(
+            interval_callback.as_ref().unchecked_ref(),
+            50,
+        );
+        
+        interval_callback.forget();
+
+    });
 
     Ok(())
 }
