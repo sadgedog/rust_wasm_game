a83c2af707585459cf2913f4cc85622b4507f680
sadgedog <sorachi1231.214@gmail.com>
2024-01-02 06:23:16 +0900

    game_loop_3

--- src/engine.rs
+++ src/engine.rs
@@ -1,17 +1,66 @@
 use crate::browser::{self, LoopClosure};
 use anyhow::{anyhow, Result};
-use futures::channel::{
-    mpsc::{unbounded, UnboundedReceiver},
-    oneshot::channel,
+use async_trait::async_trait;
+use futures::{
+    channel::{
+        mpsc::{unbounded, UnboundedReceiver},
+        oneshot::channel,
+    },
+    SinkExt,
 };
 use std::{cell::RefCell, rc::Rc, sync::Mutex};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+#[derive(Clone, Copy)]
+pub struct Point {
+    pub x: i16,
+    pub y: i16,
+}
+pub struct Renderer {
+    context: CanvasRenderingContext2d,
+}
+
+impl Renderer {
+    pub fn clear(&self, rect: &Rect) {
+        self.context.clear_rect(
+            rect.x.into(),
+            rect.y.into(),
+            rect.width.into(),
+            rect.height.into(),
+        );
+    }
+
+    pub fn draw_image(&self, image: &HtmlImageElement, frame: &Rect, destination: &Rect) {
+        self.context
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                frame.x.into(),
+                frame.y.into(),
+                frame.width.into(),
+                frame.height.into(),
+                destination.x.into(),
+                destination.y.into(),
+                destination.width.into(),
+                destination.height.into(),
+            )
+            .expect("Drawing is throwing exceptions! Unrecoverable error.");
+    }
+}
+
+#[async_trait(?Send)]
 pub trait Game {
+    async fn initialize(&self) -> Result<Box<dyn Game>>;
     fn update(&mut self);
-    fn draw(&self, context: &CanvasRenderingContext2d);
+    fn draw(&self, context: &Renderer);
 }
 
 const FRAME_SIZE: f32 = 1.0 / 60.0 * 1000.0;
@@ -22,11 +71,17 @@ pub struct GameLoop {
 
 type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 impl GameLoop {
-    pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+    pub async fn start(game: impl Game + 'static) -> Result<()> {
+        let mut game = game.initialize().await?;
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
         };
+
+        let renderer = Renderer {
+            context: browser::context()?,
+        };
+
         let f: SharedLoopClosure = Rc::new(RefCell::new(None));
         let g = f.clone();
 
@@ -37,8 +92,8 @@ impl GameLoop {
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
-            game.draw(&browser::context().expect("Context should exist"));
-            browser::request_animation_frame(f.borrow().as_ref().unwrap());
+            game.draw(&renderer);
+            let _ = browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
         browser::request_animation_frame(
             g.borrow()
--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,24 @@
 #[macro_use]
 mod browser;
 mod engine;
-
-use serde::Deserialize;
-use std::{collections::HashMap, rc::Rc, sync::Mutex};
+mod game;
+use engine::GameLoop;
+use game::WalkTheDog;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::console;
 
-#[derive(Deserialize)]
-struct Sheet {
-    frames: HashMap<String, Cell>,
-}
-
-#[derive(Deserialize)]
-struct Rect {
-    x: u16,
-    y: u16,
-    w: u16,
-    h: u16,
-}
-
-#[derive(Deserialize)]
-struct Cell {
-    frame: Rect,
-}
-
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    let document = browser::document().expect("No Document Found");
-    let canvas = document
-        .get_element_by_id("canvas")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .unwrap();
-
-    let context = browser::context().expect("Could not get browser context");
-
     browser::spawn_local(async move {
-        let sheet: Sheet = browser::fetch_json("rhb.json")
+        let game = WalkTheDog::new();
+        GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json")
-            .into_serde()
-            .expect("Could not convert rhb.json into a Sheet structure");
-
-        let image = engine::load_image("rhb.png")
-            .await
-            .expect("Could not load rhb.png");
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-            context
-                .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                    &image,
-                    sprite.frame.x.into(),
-                    sprite.frame.y.into(),
-                    sprite.frame.w.into(),
-                    sprite.frame.h.into(),
-                    300.0,
-                    300.0,
-                    sprite.frame.w.into(),
-                    sprite.frame.h.into(),
-                )
-                .expect("Could not draw sprite");
-        }) as Box<dyn FnMut()>);
-
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            )
-            .expect("Could not set interval");
-
-        interval_callback.forget();
+            .expect("Could not start game loop.");
     });
 
     Ok(())
